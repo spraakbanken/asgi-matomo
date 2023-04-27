347d5e30e34ffce4067d5bde6124b96a2f4c6963
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-04-27 10:37:56 +0200

    feat: import middleware from karp-backend
    Code taken from https://github.com/spraakbanken/karp-backend

---
+++ asgi_matomo/middleware.py
@@ -0,0 +1,175 @@
+import logging
+import random
+import time
+import typing
+from typing import Any
+import urllib.parse
+import traceback
+
+from asgiref.typing import ASGI3Application, ASGIReceiveCallable, ASGISendCallable
+from asgiref.typing import HTTPScope
+
+import httpx
+
+
+logger = logging.getLogger(__name__)
+
+
+_T = typing.TypeVar("_T")
+
+
+class _DefaultLifespan:
+    def __init__(self, app: "Router"):
+        self._app = app
+
+    async def __aenter__(self) -> None:
+        await self._app.startup()
+
+    async def __aexit__(self, *exc_info: object) -> None:
+        await self._app.shutdown()
+
+    def __call__(self: _T, app: object) -> _T:
+        return self
+
+
+class MatomoMiddleware:
+    def __init__(
+        self,
+        app: ASGI3Application,
+        *,
+        matomo_url,
+        idsite: int,
+        client: httpx.AsyncClient | None = None,
+        access_token: str | None = None,
+        assume_https: bool = True,
+        lifespan=None,
+    ) -> None:
+        self.app = app
+        self.matomo_url = matomo_url
+        self.idsite = idsite
+        self.assume_https = assume_https
+        self.access_token = access_token
+        if lifespan is None:
+            self.lifespan_context = _DefaultLifespan(self)
+        self.client = client
+
+    async def startup(self) -> None:
+        if self.client is None:
+            self.client = httpx.AsyncClient()
+
+    async def shutdown(self) -> None:
+        if self.client is not None:
+            await self.client.aclose()
+
+    async def lifespan(
+        self, scope: HTTPScope, receive: ASGIReceiveCallable, send: ASGISendCallable
+    ) -> None:
+        """
+        Handle ASGI lifespan messages, which allows us to manage application
+        startup and shutdown events.
+        """
+        started = False
+        app: typing.Any = scope.get("app")
+        await receive()
+        try:
+            async with self.lifespan_context(app) as maybe_state:
+                if maybe_state is not None:
+                    if "state" not in scope:
+                        raise RuntimeError(
+                            'The server does not support "state" in the lifespan scope.'
+                        )
+                    scope["state"].update(maybe_state)
+                await send({"type": "lifespan.startup.complete"})
+                started = True
+                await receive()
+        except BaseException:
+            exc_text = traceback.format_exc()
+            if started:
+                await send({"type": "lifespan.shutdown.failed", "message": exc_text})
+            else:
+                await send({"type": "lifespan.startup.failed", "message": exc_text})
+            raise
+        else:
+            await send({"type": "lifespan.shutdown.complete"})
+
+    async def __call__(
+        self, scope: HTTPScope, receive: ASGIReceiveCallable, send: ASGISendCallable
+    ) -> Any:
+        if scope["type"] == "lifespan":
+            await self.lifespan(scope, receive, send)
+            return
+
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        server = None
+        user_agent = None
+        accept_lang = None
+        for header, value in scope["headers"]:
+            if header == b"x-forwarded-server":
+                server = value
+            elif header == b"user-agent":
+                user_agent = value
+            elif header == b"accept_lang":
+                accept_lang = value
+
+        if server is None:
+            host, port = scope["server"]
+            server = f"{host}:{port}" if port else host
+
+        path = scope["path"]
+        if root_path := scope.get("root_path"):
+            path = f"{root_path}{path}"
+
+        url = urllib.parse.urlunsplit(
+            (
+                "https" if self.assume_https else str(scope["scheme"]),
+                server,
+                path,
+                "",
+                str(scope["query_string"]) if scope.get("query_string") else None,
+            )
+        )
+
+        cip = scope["client"][0]
+
+        start_time = time.perf_counter()
+
+        try:
+            await self.app(scope, receive, send)
+        except Exception:
+            raise
+        finally:
+            end_time = time.perf_counter()
+
+            params_that_require_token = {}
+
+            if self.access_token:
+                params_that_require_token = {
+                    "token_auth": self.access_token,
+                    "cip": cip,
+                }
+
+            tracking_dict = {
+                "idsite": self.idsite,
+                "url": url,
+                "rec": 1,
+                "rand": random.getrandbits(32),
+                "apiv": 1,
+                "ua": user_agent,
+                "gt_ms": end_time - start_time,
+                # "lang": accept_lang,
+                **params_that_require_token,
+            }
+
+            if accept_lang:
+                tracking_dict["lang"] = accept_lang
+
+            tracking_params = urllib.parse.urlencode(tracking_dict)
+            tracking_url = f"{self.matomo_url}?{tracking_params}"
+            logger.debug("Making tracking call", extra={"url": tracking_url})
+            try:
+                tracking_response = await self.client.get(tracking_url)
+            except httpx.HTTPError:
+                logger.exception("Error tracking view")
--- tests/test_asgi_matomo.py
+++ tests/test_asgi_matomo.py
@@ -1,5 +1,138 @@
+from unittest import mock
+from urllib.parse import parse_qs, urlsplit
+
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse, JSONResponse
+from starlette.exceptions import HTTPException
+from httpx import AsyncClient
+from asgi_lifespan import LifespanManager
+
+import pytest
+import pytest_asyncio
+
 from asgi_matomo import MatomoMiddleware
 
 
 def test_it_works() -> None:
     assert MatomoMiddleware is not None
+
+
+@pytest.fixture(name="matomo_client")
+def fixture_matomo_client():
+    return mock.AsyncMock(AsyncClient)
+
+
+@pytest.fixture(name="settings", scope="session")
+def fixture_settings() -> dict:
+    return {"idsite": 1, "base_url": "https://testserver"}
+
+
+@pytest.fixture(name="app")
+def create_app(matomo_client, settings: dict) -> Starlette:
+    app = Starlette()
+
+    app.add_middleware(
+        MatomoMiddleware,
+        client=matomo_client,
+        matomo_url="http://trackingserver",
+        idsite=settings["idsite"],
+    )
+
+    # @app.route("/foo")
+    async def foo(request):
+        return PlainTextResponse("foo")
+
+    # @app.route("/bar")
+    async def bar(request):
+        raise HTTPException(status_code=400, detail="bar")
+
+    async def baz(request):
+        data = await request.json()
+        return JSONResponse({"data": data})
+
+    app.add_route("/foo", foo)
+    app.add_route("/bar", bar)
+    app.add_route("/baz", baz, methods=["POST"])
+    return app
+
+
+@pytest.fixture(name="expected_q")
+def fixture_expected_q(settings: dict) -> dict:
+    return {
+        "idsite": [str(settings["idsite"])],
+        "url": [settings["base_url"]],
+        "apiv": ["1"],
+        # "lang": ["None"]
+        "rec": ["1"],
+        "ua": ["python-httpx/0.24.0"],
+    }
+
+
+@pytest_asyncio.fixture(name="client")
+async def fixture_client(app: Starlette) -> AsyncClient:
+    async with LifespanManager(app):
+        async with AsyncClient(app=app, base_url="http://testserver") as client:
+            yield client
+
+
+@pytest.mark.asyncio
+async def test_matomo_client_gets_called_on_get_foo(
+    client: AsyncClient, matomo_client, expected_q: dict
+):
+    response = await client.get("/foo")
+    assert response.status_code == 200
+
+    matomo_client.get.assert_awaited()
+
+    expected_q["url"][0] += "/foo"
+    assert_query_string(str(matomo_client.get.await_args), expected_q)
+
+
+@pytest.mark.asyncio
+async def test_matomo_client_gets_called_on_get_bar(
+    client: AsyncClient, matomo_client, expected_q: dict
+):
+    try:
+        response = await client.get("/bar")
+    except ValueError:
+        pass
+    # assert response.status_code == 200
+
+    matomo_client.get.assert_awaited()
+
+    expected_q["url"][0] += "/bar"
+
+    assert_query_string(str(matomo_client.get.await_args), expected_q)
+
+
+def assert_query_string(url: str, expected_q: dict) -> None:
+    urlparts = urlsplit(url[6:-2])
+    q = parse_qs(urlparts.query)
+    assert q.pop("rand") is not None
+    assert q.pop("gt_ms") is not None
+
+    assert q == expected_q
+
+
+@pytest.mark.asyncio
+async def test_matomo_client_gets_called_on_post_baz(
+    client: AsyncClient, matomo_client, expected_q: dict
+):
+    response = await client.post("/baz", json={"data": "content"})
+    assert response.status_code == 200
+
+    expected_q["url"][0] += "/baz"
+    matomo_client.get.assert_awaited()
+
+    assert_query_string(str(matomo_client.get.await_args), expected_q)
+
+
+# @pytest.mark.asyncio
+# async def test_matomo_client_gets_called_on_get_bar(client: AsyncClient, matomo_client):
+#     try:
+#         response = await client.get("/bar")
+#     except ValueError:
+#         pass
+#     # assert response.status_code == 200
+
+#     matomo_client.get.assert_awaited_with("http://trackingserver?")
